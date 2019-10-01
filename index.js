08ce3bc5b4468e60645b6116a98748bdf11fef8b
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-01 15:37:28 -0600

    first three done

--- index.js
+++ index.js
@@ -36,7 +36,22 @@ server.get('/api/users', (req, res) => {
     });
 });
 
-
+server.get(`/api/users/:id`, (req, res) => {
+    const id = req.params.id;
+   dataDB.findById(id)
+   .then(user => {
+       if(!user){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+       }else{
+           res.status(200).json(user);
+       }
+   })
+   .catch(error => {
+       res.status(500).json({ error: "The user information could not be retrieved." });
+   })
+});
 
 
 const port = 8000;
