303f3d020dcab29ca066ffb4acf628f0d572bdce
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-01 15:41:29 -0600

    first four done

--- index.js
+++ index.js
@@ -50,8 +50,29 @@ server.get(`/api/users/:id`, (req, res) => {
    })
    .catch(error => {
        res.status(500).json({ error: "The user information could not be retrieved." });
-   })
+   });
 });
+
+server.delete(`/api/users/:id`, (req, res) => {
+    const id = req.params.id;
+    dataDB.remove(id)
+   .then(user => {
+       if(!user){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+       }else{
+           res.status(200).json(user);
+       }
+   })
+   .catch(error => {
+       res.status(500).json({ error: "The user could not be removed" });
+   });
+});
+
+server.put(`/api/users/:id`, (req, res) => {
+    
+})
 
 
 const port = 8000;
